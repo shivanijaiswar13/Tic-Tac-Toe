1e7dae2b30cd1e984088dc1d5b84365c47af048f
shivanijaiswar13 <shivanijaiswar01@gmail.com>
2026-03-20 16:13:49 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,12 @@
+let boxes = document.querySelectorAll(".box");
+let resetBtn = document.querySelector("#reset-btn");
+
+let turnO = true;
+
+const winPatterns = [
+    [0,1,2],
+    [0,3,6],
+    [0,4,8],
+    [1,4,7],
+    []
+]
