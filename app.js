757147f5f0c82fe4b3bfe1abeaf81345b78ff15b
shivanijaiswar13 <shivanijaiswar01@gmail.com>
2026-03-20 16:55:17 +0530

    logic build

--- app.js
+++ app.js
@@ -8,5 +8,34 @@ const winPatterns = [
     [0,3,6],
     [0,4,8],
     [1,4,7],
-    []
+    [2,5,8],
+    [2,4,6],
+    [3,4,5],
+    [6,7,8]
 ]
+
+boxes.forEach((box)=>{
+    box.addEventListener("click",()=>{
+    console.log("box was clicked");
+    if(turnO){ //player O
+        box.innerText = "O";
+        turnO = false;
+    }else{ //player X
+         box.innerText = "X";
+         turnO = true;
+    }
+    box.disabled = true;
+
+    checkWinner();
+    })
+})
+
+const checkWinner = ()=>{
+    for(let pattern of winPatterns){
+        
+        let pos1Val = boxes[pattern[0]].innerText;
+        let pos2val = boxes[pattern[1]].innerText;
+        let pos3val = boxes[pattern[2]].innerText;
+        if(pos1Val != ""&& pos2val!= "" && pos3val!="")
+    }
+}
